52127320d2f2ddb21792633d431b7205c27aeb56
stephenjude <stephenjudesuccess@gmail.com>
2022-12-02 23:14:33 +0100

    DONE: added more account actions: freeze, unfreeze, close, reopen

--- src/Resources/Account.php
+++ src/Resources/Account.php
@@ -33,4 +33,40 @@ class Account extends ApiResource
     public string $createdAt;
 
     public string $updatedAt;
+
+    public function freeze(?string $reason = null): bool
+    {
+        $payload = array_filter(['reason' => $reason]);
+
+        $response = $this->bloc->put("accounts/$this->id/freeze", $payload);
+
+        return (bool) $response['success'];
+    }
+
+    public function unfreeze(?string $reason = null): bool
+    {
+        $payload = array_filter(['reason' => $reason]);
+
+        $response = $this->bloc->put("accounts/$this->id/unfreeze", $payload);
+
+        return (bool) $response['success'];
+    }
+
+    public function close(?string $reason = null): bool
+    {
+        $payload = array_filter(['reason' => $reason]);
+
+        $response = $this->bloc->put("accounts/$this->id/close", $payload);
+
+        return (bool) $response['success'];
+    }
+
+    public function reopen(?string $reason = null): bool
+    {
+        $payload = array_filter(['reason' => $reason]);
+
+        $response = $this->bloc->put("accounts/$this->id/reopen", $payload);
+
+        return (bool) $response['success'];
+    }
 }
