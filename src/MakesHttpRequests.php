3ff8eb8bdac8cb7ea1f51355c591f9c0ebfaebf7
stephenjude <stephenjudesuccess@gmail.com>
2022-12-12 10:29:03 +0100

    fixed Call to protected method error

--- src/MakesHttpRequests.php
+++ src/MakesHttpRequests.php
@@ -12,27 +12,27 @@ use Stephenjude\BlocHqPhpSdk\Exceptions\UnauthorizedException;
 
 trait MakesHttpRequests
 {
-    protected function get(string $uri)
+    public function get(string $uri)
     {
         return $this->request('GET', $uri);
     }
 
-    protected function post(string $uri, array $payload = [])
+    public function post(string $uri, array $payload = [])
     {
         return $this->request('POST', $uri, $payload);
     }
 
-    protected function put(string $uri, array $payload = [])
+    public function put(string $uri, array $payload = [])
     {
         return $this->request('PUT', $uri, $payload);
     }
 
-    protected function delete(string $uri, array $payload = [])
+    public function delete(string $uri, array $payload = [])
     {
         return $this->request('DELETE', $uri, $payload);
     }
 
-    protected function request(string $verb, string $uri, array $payload = [])
+    public function request(string $verb, string $uri, array $payload = [])
     {
         $response = $this->client->request(
             $verb,
@@ -58,7 +58,7 @@ trait MakesHttpRequests
         return (int)substr($response->getStatusCode(), 0, 1) === 2;
     }
 
-    protected function handleRequestError(ResponseInterface $response): void
+    public function handleRequestError(ResponseInterface $response): void
     {
         match ($response->getStatusCode()) {
             400 => throw new BadRequestException((string)$response->getBody()),
